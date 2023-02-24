ae367a29eb22ba43c35d6ceef6c7b8acd5416c39
Jani Cho <janicho117@gmail.com>
2023-02-24 11:21:43 +0800

    modified index banner

--- banner.js
+++ banner.js
@@ -1,22 +1,37 @@
 $(document).ready(function(){
   $('.carousel').slick({
-  slidesToShow: 3,
+  slidesToShow: 1,
   infinite: true,
-  // autoplay: true,
+
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '20px',
   autoplaySpeed: 3000,
   arrows: true,
   dots: true,
+  variableWidth: true,
   responsive: [
 
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 2
-      }
+        slidesToScroll: 2,
+        variableWidth: true,
+        centerPadding: '80px',
+        
+      },
+
     },
   ]
   });
 });
+
+$(".carousel").on("init", function (event, slick) {
+  var numSlides = slick.slideCount;
+  console.log(numSlides);
+});
+
+$('.carousel').slick('getSlick').slideCount
 
 // Slick version 1.5.8
